1972db0628d0438dd197e04bce99a15db60231d4
Nxttxrxl <anouarkajajrosario@gmail.com>
2026-01-21 05:42:56 +0100

    Migración a Vercel: Instalación de adaptador @astrojs/vercel y configuración

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  // output: 'static', // Default is static, explicitly creating a static build compatible with Vercel
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
